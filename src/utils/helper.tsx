ef803aed398d6726a01833c4dbf1ec0c4a8ed281
Tuan Pham <tuan.pm@kovapaint.com>
2025-02-01 09:07:12 +0700

    update

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,3 +1,4 @@
 export function isMobileOrSmallScreen(): boolean {
+    if (typeof window === "undefined") return false;
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || window.innerWidth < 1200;
 }
